59b6c1c4d748c4b3b0d0ae146e91657dfec3c652
ZaydenF <zaydenfleming@gmail.com>
2024-09-03 13:43:16 -0700

    Show the next question after right answer

--- myscript.js
+++ myscript.js
@@ -2,5 +2,16 @@ $(document).ready(function() {
 		$("#right1").click(function(){
 			$(".q1").show();
 		});
-		$
+		$(".right2").click(function(){
+			$(".q2").show();
 		});
+		$(".right3").click(function(){
+			$(".q3").show();
+		});
+		$(".right4").click(function(){
+			$(".q4").show();
+		});
+		$(".right5").click(function(){
+			$("#submit").show();
+		});
+	});
